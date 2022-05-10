55636c9b8a2197dda58769ff5cae436522a23e3c
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-05-10 13:23:04 +0800

    updated settings

--- my_site/settings.py
+++ my_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config.DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.DJANGO_DEBUG
 
-ALLOWED_HOSTS = ['anya-site.herokuapp.com']
+ALLOWED_HOSTS = ['anya-site.herokuapp.com', 'localhost']
 
 
 # Application definition
